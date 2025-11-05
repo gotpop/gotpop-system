8d6c74a8be80a70b2e842c56dc98648b9fd1740e
GotPop <gotpop@users.noreply.github.com>
2025-11-05 16:49:25 +0000

    Preparing package for publishing

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,12 +2,12 @@
 // Re-export all components, utilities, types, and other modules
 
 export * from "./components"
-export * from "./types"
-export * from "./utils"
-export * from "./lib"
 export * from "./constants"
 export * from "./hooks"
+export * from "./lib"
 export * from "./providers"
+export * from "./types"
+export * from "./utils"
 
 // Export styles directory for consumers who need direct CSS access
 // Note: CSS files are imported directly in components, but this allows
