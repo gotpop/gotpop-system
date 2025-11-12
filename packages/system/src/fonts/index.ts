82d44f124db95ef5eb5697d46a5b065540da9538
GotPop <gotpop@users.noreply.github.com>
2025-11-12 00:33:24 +0000

    Updating fonts

---
+++ packages/system/src/fonts/index.ts
@@ -0,0 +1,13 @@
+import localFont from "next/font/local"
+
+export const inter = localFont({
+  src: "./inter/Inter.woff2",
+  variable: "--font-inter",
+  display: "swap",
+})
+
+export const monaspace = localFont({
+  src: "./monaspace/Monaspace.woff2",
+  variable: "--font-monaspace",
+  display: "swap",
+})
