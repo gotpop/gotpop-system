e7f3cc0efb89813b5f5d339bf874fd17f082206c
GotPop <gotpop@users.noreply.github.com>
2025-11-14 15:04:25 +0000

    Use direct import

--- packages/system/src/utils/index.ts
+++ packages/system/src/utils/index.ts
@@ -2,5 +2,3 @@ export * from "./cn"
 export * from "./createStoryblokServerComponent"
 export * from "./date-formatter"
 export * from "./storyblok"
-// Note: storyblok-utils is server-only and should be imported directly
-// import { getStoryPath } from "@gotpop/system/utils/storyblok-utils"
